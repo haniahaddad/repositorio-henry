33e1a1ef08010e8134367ba9dd5a3460106b7627
haniahaddad <haniaelizahaddad@hotmail.com>
2022-12-14 19:52:31 -0600

    tarea2

---
+++ archivo-ejemplo.js
@@ -0,0 +1,9 @@
+// variables: sirve para guardar datos para despues usarlos.
+
+// strings: es texto 
+
+// funciones: son codigos que realiza lo que le estas pidiendo.
+
+// declaraciones: determina el nombre y el tipo de datos
+
+// valores booleanos: representa un valor de verdedero o falso 
